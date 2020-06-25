136d598127065b551afd17fa60114c7470e27820
Breno Duarte <jbrenodm@pop-os.localdomain>
2020-06-25 12:08:12 -0300

    add versao do php semi completa

--- host_container/projetos/php7/index.php
+++ host_container/projetos/php7/index.php
@@ -4,12 +4,12 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Servidor PHP 7</title>
+    <title>Servidor PHP 7.3</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
 </head>
 <body>
     <?php
-        echo "<h3>Servidor PHP 7 criado por: Breno Duarte - jbrenodm@gmail.com</h3>";
+        echo "<h3>Servidor PHP 7.3 criado por: Breno Duarte - jbrenodm@gmail.com</h3>";
         echo "<br>";
         echo "<a href='phpinfo.php'>Visualizar o <b>phpnfo();</b></a>";
     ?>
