470f2ae83485a146030f744d5324516c8df3b380
Breno Duarte <jbrenodm@pop-os.localdomain>
2020-06-25 12:06:54 -0300

    Atualizado tela de inicio do server docker pessoal

--- host_container/projetos/php7/index.php
+++ host_container/projetos/php7/index.php
@@ -1,3 +1,17 @@
-<?php
-    echo "Servidor PHP 7 criado por: Breno Duarte - jbrenodm@gmail.com"    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servidor PHP 7</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+</head>
+<body>
+    <?php
+        echo "<h3>Servidor PHP 7 criado por: Breno Duarte - jbrenodm@gmail.com</h3>";
+        echo "<br>";
+        echo "<a href='phpinfo.php'>Visualizar o <b>phpnfo();</b></a>";
+    ?>
+</body>
+</html>
